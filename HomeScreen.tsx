a8a4fa9580e56af8580f2141205a04c5d68eceee
MantisClone <david.huntmateo@request.network>
2024-06-18 18:42:28 -0400

    Switch the paymentAddress (payment recipient) to the payeeAddress

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -73,7 +73,7 @@ export const HomeScreen = () => {
         payerIdentity: sendUserAddress!,
         signerIdentity: account?.address!,
         expectedAmount: parseUnits(amount, 6).toString(),
-        paymentAddress: sendUserAddress!,
+        paymentAddress: account?.address!,
         reason: "Reason",
         currencyAddress: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8",
         chain: sepolia,
